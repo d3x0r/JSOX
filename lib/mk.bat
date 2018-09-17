ed5bfabcf1b1ababb257c4c495da51d605a86b21
d3x0r <d3x0r@no-reply.github.com>
2018-09-17 08:17:38 -0700

    gzip'd and transpoiled

--- lib/mk.bat
+++ lib/mk.bat
@@ -1,2 +1,4 @@
 call google-closure-compiler.cmd --language_out NO_TRANSPILE   --js=jsox.js --js_output_file=jsox.es6.js 
 call google-closure-compiler.cmd --language_out ECMASCRIPT3   --js=jsox.js --js_output_file=jsox.es3.js 
+gzip -f jsox.es6.js
+gzip -f jsox.es3.js
