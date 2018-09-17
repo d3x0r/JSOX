3ef92a1c7b6fcced1c7f4ead3b81c9de016c2dac
d3x0r <d3x0r@no-reply.github.com>
2018-09-17 08:07:43 -0700

    Add a minimized verzion and an es3 transpile

---
+++ lib/mk.bat
@@ -0,0 +1,2 @@
+call google-closure-compiler.cmd --language_out NO_TRANSPILE   --js=jsox.js --js_output_file=jsox.es6.js 
+call google-closure-compiler.cmd --language_out ECMASCRIPT3   --js=jsox.js --js_output_file=jsox.es3.js 
