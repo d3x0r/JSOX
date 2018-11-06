656d27c14345717048605c8a3737e6bc72850407
d3x0r <d3x0r@no-reply.github.com>
2018-11-06 06:42:11 -0800

    Improve benchmark

--- tests/benchmark/bench1.js
+++ tests/benchmark/bench1.js
@@ -1,4 +1,12 @@
 var JSOX=require("../../lib/jsox.js" );
+var JSON5
+try {
+JSON5=require("json5" );
+}catch(err){}
+var JSON6;
+try {
+JSON6=require("json6" );
+}catch(err){}
 //var JSOX=require("../../lib/jsox.es6.pretty.js" );
 var sf = JSOX.stringifier();
 
@@ -30,8 +38,42 @@ function runParser( l, p ) {
 
 var x = runParser( "JSOX", JSOX );
 var n = runParser( "JSON", JSON );
+if( JSON5 )
+var n5 = runParser( "JSON5", JSON5 );
+if( JSON6 )
+var n6 = runParser( "JSON5", JSON6 );
 
-console.log( x.sn/n.sn);
-console.log( x.pn/n.pn);
-console.log( n.sn/x.sn);
-console.log( n.pn/x.pn);
+console.log( "Node JSON v JSOX" );
+console.log( "stringify", n.sn/x.sn);
+console.log( "parse    ", n.pn/x.pn);
+
+if( JSON5 ) {
+console.log( "Node JSON v JSON5" );
+console.log( "stringify", n.sn/n5.sn);
+console.log( "parse    ", n.pn/n5.pn);
+}
+
+if( JSON6 ) {
+console.log( "Node JSON v JSON6" );
+console.log( "stringify", n.sn/n6.sn);
+console.log( "parse    ", n.pn/n6.pn);
+}
+
+if( JSON5 ) {
+console.log( "JSON5 v JSOX" );
+console.log( "stringify", n5.sn/x.sn);
+console.log( "parse    ", n5.pn/x.pn);
+}
+
+if( JSON5 && JSON6 ) {
+console.log( "JSON5 v JSON6" );
+console.log( "stringify", n5.sn/n6.sn);
+console.log( "parse    ", n5.pn/n6.pn);
+}
+
+
+if( JSON6 ) {
+console.log( "JSON6 v JSONX" );
+console.log( "stringify", n6.sn/x.sn);
+console.log( "parse    ", n6.pn/x.pn);
+}
