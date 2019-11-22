ff84057e40cfe16c09d531290b253290a70a7102
d3x0r <d3x0r@no-reply.github.com>
2019-11-21 23:56:40 -0800

    Fix typed array revive test

--- tests/testTypeArray.js
+++ tests/testTypeArray.js
@@ -1,11 +1,11 @@
 var JSOX=require( ".." );
 
-console.log( "MNOP=", JSOX.parse( '{ab:ab[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:u8[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:u16[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:u32[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:s8[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:s16[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:s32[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:f32[0,1,2,3,4,5,6,7]}' ) );
-console.log( "MNOP=", JSOX.parse( '{ab:f64[0,1,2,3,4,5,6,7]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:ab["0123456"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:u8["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:u16["01234565345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:u32["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:s8["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:s16["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:s32["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:f32["0123456789012345"]}' ) );
+console.log( "MNOP=", JSOX.parse( '{ab:f64["00123456789012345123456789012345"]}' ) );
