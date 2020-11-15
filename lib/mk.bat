f9ba71d989abb28ea1f832b0f437f22c1e7483f9
d3x0r <d3x0r@no-reply.github.com>
2020-11-15 13:50:50 -0800

    Update built product and build scripts; es5 setters/getters don't transpile to 3; but I don't *NEED* ES3 anymore

--- lib/mk.bat
+++ lib/mk.bat
@@ -1,7 +1,7 @@
 call google-closure-compiler.cmd --language_out NO_TRANSPILE   --js=jsox.js --js_output_file=jsox.es6.js 
-call google-closure-compiler.cmd --language_out ECMASCRIPT3   --js=jsox.js --js_output_file=jsox.es3.js 
-call google-closure-compiler.cmd --language_out ECMASCRIPT3 --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es3.pretty.js 
+:call google-closure-compiler.cmd --language_out ECMASCRIPT3   --js=jsox.js --js_output_file=jsox.es3.js 
+:call google-closure-compiler.cmd --language_out ECMASCRIPT3 --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es3.pretty.js 
 call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es6.pretty.js 
 
 gzip -f jsox.es6.js
-gzip -f jsox.es3.js
+:gzip -f jsox.es3.js
