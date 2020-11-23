10c9fea216d650aee897d6e054312305304c0b3e
d3x0r <d3x0r@no-reply.github.com>
2020-11-23 03:23:14 -0800

    Added automated test cases for last failed tests

---
+++ test/2.101.js
@@ -0,0 +1,20 @@
+'use strict';
+const JSOX = require( ".." );
+
+
+describe('Added in 1.2.101', function () {
+
+	it( 'Parses short unquoted value',  function() {
+
+		expect( JSOX.parse( '{fName:Test,fmtName:"",lName:User,mName:M,nName:"",suffix:"",title:"Mr.",webAccounts:[]}' ) )
+			.to.deep.equal( {fName:Test,fmtName:"",lName:User,mName:M,nName:"",suffix:"",title:"Mr.",webAccounts:[]} );
+	} );
+	it( 'handles keywords with spaces', function() {
+		expect( JSOX.parse( "[null,null,null, null , null,null ,null]" ) )
+			.to.deep.equal( [null,null,null, null , null,null ,null] );
+	} );
+
+	// references which revived deeply in the current stack.... 
+	// node ../tests/data/rawtest.js ./testReferenceRevive.jsox
+
+} );
