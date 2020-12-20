8b677f59935d95ab0e6f4c1f5fc61ffbadc7a3eb
d3x0r <d3x0r@no-reply.github.com>
2020-12-19 17:15:00 -0800

    Adjust test since other things work better now

--- test/1.2.105.mjs
+++ test/1.2.105.mjs
@@ -24,7 +24,9 @@ World.fromString = function( field, value ) {
 		// would have to trust that you somehow kept the value, but this does not assign
 		// the field on returning; which also shouldn't FAIL (previous to 1.2.105 pushing array content
 		// to new 'undefined' value did not work, and results with an obscure thrown error)
-		return undefined;
+		if( field === "walls" )
+			return undefined;
+		return value;
 		// the proper result would be something like... 
 		//return this[field] = [];
 		// Or maybe from some other source...
@@ -48,7 +50,7 @@ JSOX.addType( "~T", Texture );
 function Name() {}
 JSOX.addType( "~N", Name );
 
-describe('Added in 1.2.102', function () {
+describe('Added in 1.2.105', function () {
 	it( 'Handles Arrays of Typed Items', function() {
 		const str1 = '{op:world,world:~Wr{lines:[~L{from:-5,id:0,r:{n:v3{x:0,y:1,z:0},o:v3{x:-5,y:0,z:0}},to:5},~L{from:-5,id:1,r:{n:v3{x:1,y:0,z:0},o:v3{x:0,y:5,z:0}},to:5},~L{from:-5,id:2,r:{n:v3{x:1,y:0,z:0},o:v3{x:0,y:-5,z:0}},to:5},~L{from:-5,id:3,r:{n:v3{x:0,y:1,z:0},o:v3{x:5,y:0,z:0}},to:5}],names:[~N{flags:{vertical:false},id:0,name:Default}],sectors:[~S{id:0,name:null,r:{n:v3{x:0,y:0,z:1},o:v3{x:0,y:0,z:0}},texture:~T{flags:{color:true},name:ref["world","names",0]},wall:~Wl{end:~Wl{end:~Wl{end:ref["world","sectors",0,"wall","end"],end_at_end:true,id:3,into:null,line:ref["world","lines",3],start:~Wl{end:ref["world","sectors",0,"wall","end","end"],end_at_end:false,id:2,into:null,line:ref["world","lines",2],start:ref["world","sectors",0,"wall"],start_at_end:false},start_at_end:true},end_at_end:false,id:1,into:null,line:ref["world","lines",1],start:ref["world","sectors",0,"wall"],start_at_end:true},end_at_end:false,id:0,into:null,line:ref["world","lines",0],start:ref["world","sectors",0,"wall","end","end","start"],start_at_end:false}}],walls:[ref["world","sectors",0,"wall"],ref["world","sectors",0,"wall","end"],ref["world","sectors",0,"wall","end","end","start"],ref["world","sectors",0,"wall","end","end"]]}}'
 		const obj1 = JSOX.parse( str1 );
@@ -195,12 +197,6 @@ describe('Added in 1.2.102', function () {
                start_at_end: false
             }
          }
-],
-      walls: [
-         ref["world","sectors",0,"wall"],
-         ref["world","sectors",0,"wall","end"],
-         ref["world","sectors",0,"wall","end","end","start"],
-         ref["world","sectors",0,"wall","end","end"]
 ]
    }
 }`
@@ -212,7 +208,7 @@ describe('Added in 1.2.102', function () {
 		const str = '{op:world,world:~Wr{lines:[{flags:{vertical:false},id:0,name:"Default"}],names:ref["lines"]}}'
 		expect( function() {
 	                const obj = JSOX.parse( str );
-        	        //console.log( "OBJ" );
+        	        console.log( "OBJ" );
 		// Error is like 'Path did not resolve properly.lines at lines(0)'
 		} ).to.throw( Error );
         } );
