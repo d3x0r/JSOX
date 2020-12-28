ebf4c472ad1b0fac925c6d97a3f78b202753ee49
d3x0r <d3x0r@no-reply.github.com>
2020-12-27 16:01:17 -0800

    Added test - passes this version

--- test/sack-vfs-1.0.1007.mjs
+++ test/sack-vfs-1.0.1007.mjs
@@ -1,11 +1,13 @@
 import {JSOX} from "../lib/jsox.mjs";
 
 
-describe('Added in 1.2.106', function () {
+describe('Added in 1.0.1007', function () {
 
 
 
 	it( 'spaces around colons?', function() {
+		//console.log( "Parsed:", JSOX.parse( '{\n\t"a" : "b", "c" : "d" }' ) )
+		//console.log( "Parsed:", JSOX.parse( '{\n\t"a" : "b", "c" : "d" }' ) )
 		expect( JSOX.parse( '{"a" : "b", "c" : "d" }' ) )
                        .to.deep.equal( {a:"b",c:"d"} );
 //		console.log( "Fail:", obj );
