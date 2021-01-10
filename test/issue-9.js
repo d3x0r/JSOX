f5ea25632ac4af67966e0762ccfacaff7cc3889d
d3x0r <d3x0r@no-reply.github.com>
2021-01-09 18:54:20 -0800

    Remove noisy logging; do keep the rename of the test

--- test/issue-9.js
+++ test/issue-9.js
@@ -6,11 +6,9 @@ const JSOX = require( ".." );
 describe('Issue 9', function () {
 
 	it( 'parses 1',  function() {
-			console.log( JSOX.parse('{a:"",b:[{c:[{d:false}]}],e:"TEST"}' ), JSOX.stringify(JSOX.parse('{a:"",b:[{c:[{d:false}]}],e:"TEST"}' )) );
         	expect( JSOX.parse('{a:"",b:[{c:[{d:false}]}],e:"TEST"}' )).to.deep.equal({a:"",b:[{c:[{d:false}]}],e:"TEST"}) 
 	} );
 	it( 'parses 2',  function() {
-			console.log( JSOX.parse('{a:"",b:[{c:[{d:false}]}],e:"TEST"}' ), JSOX.stringify(JSOX.parse('{a:"",b:[{c:[{d:false}]}],e:"TEST"}' )) );
         	expect( JSOX.parse('{a:"",b:[{c:[{d:false}]}]}' )).to.deep.equal({a:"",b:[{c:[{d:false}]}]}) 
 	} );
         
