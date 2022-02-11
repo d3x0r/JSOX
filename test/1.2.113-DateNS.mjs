5e31a05b1eaa0323c4b460d40be84103bb18bc29
d3x0r <d3x0r@no-reply.github.com>
2022-02-11 00:16:36 -0800

    Add tests to parse and revive into DateNS to preserve timestamp precision

---
+++ test/1.2.113-DateNS.mjs
@@ -0,0 +1,19 @@
+'use strict';
+import {JSOX} from "jsox";
+
+
+describe('Added in 1.2.113 (DateNS)', function () {
+
+	it( 'handles stringify', function() {
+        	const str = JSOX.stringify( new JSOX.DateNS( "2022-01-01T00:00:00Z", 123 ) );
+                expect( str ).to.equal( "2021-12-31T16:00:00.000000123-08:00" );
+        } );
+
+	it( 'handles parse', function() {
+        	const val = JSOX.parse( "2022-01-01T00:00:00.000000123Z" );
+		const strval = ''+val.toISOString()+val.ns;
+		expect( strval ).to.equal( "2022-01-01T00:00:00.000Z123" );
+        } );
+
+})
+
