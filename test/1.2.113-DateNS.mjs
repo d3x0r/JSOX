2a5b9f32753cca5577e7440eb64d0d28b1d9d398
d3x0r <d3x0r@no-reply.github.com>
2022-04-08 00:03:22 -0700

    Update config so test can set the timezone consistantly

--- test/1.2.113-DateNS.mjs
+++ test/1.2.113-DateNS.mjs
@@ -4,9 +4,14 @@ import {JSOX} from "jsox";
 
 describe('Added in 1.2.113 (DateNS)', function () {
 
-	it( 'handles stringify', function() {
+	it( 'handles stringify (with passed UTC timezone)', function() {
         	const str = JSOX.stringify( new JSOX.DateNS( "2022-01-01T00:00:00Z", 123 ) );
                 expect( str ).to.equal( "2021-12-31T16:00:00.000000123-08:00" );
+        } );
+
+	it( 'handles stringify (with timezone offset)', function() {
+        	const str = JSOX.stringify( new JSOX.DateNS( "2022-01-01T00:00:00-05:00", 123 ) );
+                expect( str ).to.equal( "2021-12-31T21:00:00.000000123-08:00" );
         } );
 
 	it( 'handles parse', function() {
