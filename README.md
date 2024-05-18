d4ba35f0726d15a25048ec1ec2309b1af189d6de
d3x0r <d3x0r@no-reply.github.com>
2024-05-17 22:29:51 -0700

    For some reason bootstrap wants a file called 'node' instead of a proper extension

--- README.md
+++ README.md
@@ -806,6 +806,9 @@ The product of this should run on very old platforms also, especially `node_modu
 ## Changelog
 - 1.2.120 (inprogress)
     - Add JSDoc comments to provide some type information.
+    - fix quoting the string `'[]'`; ended up unquoted.
+    - Fragmented changelog.
+    - Change external testing tool.
 - 1.2.119
     - Provide typescript definitions.
     - Provide JSOX as a import name; see issue #13.
--- test/1.2.113-DateNS.js
+++ test/1.2.113-DateNS.js
@@ -2,10 +2,15 @@
 const JSOX = require( "../lib/jsox.js" );
 
 describe('Added in 1.2.113 (DateNS)', function () {
-
+	const t = JSOX.DateNS("2022-01-01T00:00:00Z").toISOString();
+	const plus = t.lastIndexOf( "+" );
+	const minus = t.lastIndexOf( "-" );
+	const Z = t.lastINdexOf( "Z" );
+	const pos = ( plus > minus )?(plus>Z)?plus:Z:(minus>Z)?minus:Z;
+	const tz = t.substr( pos );
 	it( 'handles stringify', function() {
         	const str = JSOX.stringify( new JSOX.DateNS( "2022-01-01T00:00:00Z", 123 ) );
-                expect( str ).to.equal( "2021-12-31T16:00:00.000000123-08:00" );
+                expect( str ).to.equal( "2021-12-31T16:00:00.000000123" + tz );
         } );
 
 	it( 'handles parse', function() {
