c90cbe37e05b672122e99a370c3a5d529bc70781
d3x0r <d3x0r@no-reply.github.com>
2024-10-15 01:21:38 -0700

    Updated version and build products

--- lib/mk.bat
+++ lib/mk.bat
@@ -1,7 +1,9 @@
-call google-closure-compiler.cmd --language_out NO_TRANSPILE   --js=jsox.js --js_output_file=jsox.es6.js 
+:call google-closure-compiler.cmd --language_out NO_TRANSPILE   --js=jsox.js --js_output_file=jsox.es6.js 
 :call google-closure-compiler.cmd --language_out ECMASCRIPT3   --js=jsox.js --js_output_file=jsox.es3.js 
 :call google-closure-compiler.cmd --language_out ECMASCRIPT3 --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es3.pretty.js 
-call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es6.pretty.js 
+:call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es6.pretty.js 
 
-gzip -f jsox.es6.js
+gzip -n -f jsox.es6.js
+gzip -n -f jsox.min.mjs
+gzip -n -f jsox.min.js
 :gzip -f jsox.es3.js
