14294eb901c5667f74eb89a965c17ee39ab3b685
d3x0r <d3x0r@no-reply.github.com>
2025-01-17 02:14:46 -0800

    Update build script for min versions; update gzipped versions

--- lib/mk.bat
+++ lib/mk.bat
@@ -3,7 +3,12 @@
 :call google-closure-compiler.cmd --language_out ECMASCRIPT3 --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es3.pretty.js 
 :call google-closure-compiler.cmd --language_out NO_TRANSPILE --formatting=pretty_print --js=jsox.js --js_output_file=jsox.es6.pretty.js 
 
-gzip -n -f jsox.es6.js
 gzip -n -f jsox.min.mjs
 gzip -n -f jsox.min.js
+
+git add jsox.min.mjs
+git add jsox.min.js
+
+git checkout jsox.min.mjs
+git checkout jsox.min.js
 :gzip -f jsox.es3.js
